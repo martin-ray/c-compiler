3a1e7cb5d38940f3237f1a9c3dfbeb4d552748a2
aoyagi ray <aorayagi@gmail.com>
2022-06-30 03:51:00 +0000

    made a compiler compiling an integer

---
+++ chapter2/test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+assert(){
+    expected="$1"
+    input="$2"
+
+    ./9cc "$input" > tmp.s
+    gcc -o tmp tmp.s
+    ./tmp
+    actual="$?"
+
+    if [ "$actual" = "$expected" ];then
+        echo "$input => $actual"
+    else
+        echo "$input => $expected expected, but got $actual"
+        exit 1
+    fi
+}
+
+assert 0 0
+assert 42 42
+assert 41 41
+assert 77 77
+assert 55 55
+assert 122 122
+echo OK
